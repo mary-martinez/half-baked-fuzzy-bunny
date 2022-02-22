5350b41dcd6bbafa1daf42d3c6e3f36e530d63ff
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-21 20:28:23 -0800

    fixing eslint foreverrr

--- fetch-utils.js
+++ fetch-utils.js
@@ -16,7 +16,7 @@ export async function getFamilies() {
 
 export async function deleteBunny(id) {
     // delete a single bunny using the id argument
-    const response = await client.from('fuzzy_bunnies').delete().match({id});
+    const response = await client.from('fuzzy_bunnies').delete().match({ id });
     return checkError(response);
 }
 
